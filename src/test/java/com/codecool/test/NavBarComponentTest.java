fcfc14e50b16a171b03bdcc83a91d46669b396ad
Zsofia Eszter <57215326+E-Zsofi@users.noreply.github.com>
2024-10-17 09:32:12 +0200

    navbarcomponent page is done, tests created for the component

--- src/test/java/com/codecool/test/NavBarComponentTest.java
+++ src/test/java/com/codecool/test/NavBarComponentTest.java
@@ -6,15 +6,10 @@ import org.junit.jupiter.api.AfterEach;
 import org.junit.jupiter.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
-import org.openqa.selenium.WebDriver;
-import org.openqa.selenium.support.ui.WebDriverWait;
 
 
 public class NavBarComponentTest extends BaseTest {
-    private WebDriver driver;
-    private WebDriverWait wait;
     private NavBarComponent navBarComponent;
-
 
     @BeforeEach
     public void setUpProject() {
